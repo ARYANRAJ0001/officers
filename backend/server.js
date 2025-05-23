d8f2b7f40d55a959e923389a5673fddac1e2f4bb
ARYANRAJ0001 <23it3013@rgipt.ac.in>
2025-05-23 13:17:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const path = require('path');
-const helmet = require('helmet');
+
 require('dotenv').config();
 
 const app = express();
